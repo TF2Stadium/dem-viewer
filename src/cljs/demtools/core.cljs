429f9648fb8424b089808bed692306ea160745a7
George Pittarelli <george.pittarelli@massdrop.com>
2017-03-20 22:40:11 -0700

    Perf improvements

--- src/cljs/demtools/core.cljs
+++ src/cljs/demtools/core.cljs
@@ -21,9 +21,11 @@
 (def send-chan (chan))
 
 (def app-state
-  (atom {:title "Hello Chestnut!"
-         :packets nil
+  (atom {:packets nil
+         :selected-idx nil
          :count 0
+         :limit 50
+         :offset 0
          :file/results ""}))
 
 (def w (js/Worker. "js/compiled/demtools-worker.js"))
@@ -31,19 +33,20 @@
       (fn [& e] (js/console.log "received" e)))
 (.postMessage w "hi guy")
 
-
 (defmulti read (fn [& args] (namespace (apply om/dispatch args))))
 (defn direct-read [state k] {:value (get @state k nil)})
 (defmethod read nil [{:keys [state] :as env} k {:keys [idx file offset limit]}]
   (case k
-    :packets
-    {:value (->> (or (get @state :packets []) [])
-                 (drop offset)
-                 (take limit)
-                 vec)}
+    ;;    :visible-packets
 
-    :packet (when idx {:value (get (get @state :packets []) idx)})
-    :packets-count {:value (count (get @state :packets))}
+    ;;     :packets
+    ;;     (let [packets (or (get @state :packets []) [])
+    ;;           last-idx (max 0 (dec (count packets)))
+    ;;           start (min offset last-idx)
+    ;;           end (min (+ limit offset) last-idx)]
+    ;;       {:value (subvec packets start end)})
+    ;;     :packet (when idx {:value (get (get @state :packets []) idx)})
+    ;;     :packets-count {:value (count (get @state :packets))}
 
     (direct-read state k)))
 (defmethod read "file"
@@ -55,8 +58,14 @@
       (merge {:value old-file}
              (when (not= old-file file) {:file ast})))))
 
-(defn mutate [{:keys [state] :as env} key params]
-  (cond true {:value nil}))
+(defn mutate [{:keys [state] :as env} key {:keys [value]}]
+  (cond
+    #{`selected-idx `offset}
+    (let [state-key (-> key name keyword)]
+      {:value {:keys [state-key]}
+       :action #(swap! state assoc state-key value)})
+
+    true {:value (swap! state assoc key value)}))
 
 (defn send-to-chan [c]
   (fn [{:keys [file] :as data} cb]
@@ -65,7 +74,7 @@
             query (get-in file [:params :file/file])]
         (put! c [query cb])))))
 
-(defn setImmediate [fn] (js/setTimeout fn 1000))
+(defn setImmediate [fn] (js/setTimeout fn 20))
 
 ;; parse the next message, return a list of packets; or nil if we're done
 (defn parse-1 [parser]
@@ -91,11 +100,10 @@
             (if (and packets (< @idx n))
               (recur)
               @idx)))]
-    (js/console.log n real-len)
     (set! (.-length chunk) real-len)
-    chunk))
+    (vec chunk)))
 
-(def ^:private parse-chunk-size 500)
+(def ^:private parse-chunk-size 1000)
 (defn parse-loop-async [parser output-chan]
   (let [packets (parse-n parser parse-chunk-size)]
     (when packets
@@ -121,6 +129,160 @@
 
     (.readAsArrayBuffer fr file)
     output))
+
+(defn process-loop [control-chan]
+  (go-loop [current-cb nil
+            current-file nil
+            current-parse-chan nil
+            current-packets nil]
+    (let [[m port] (alts! (remove nil? [control-chan current-parse-chan]))]
+      (cond
+        (= port control-chan)
+        (let [[file cb] m]
+          (if (and file (not= file current-file))
+            (do (when current-parse-chan (close! current-parse-chan))
+                (cb {:packets []})
+                (recur cb file (when file (parse file)) []))
+            (recur current-cb current-file current-parse-chan current-packets)))
+
+        (= port current-parse-chan)
+        (do
+          (let [new-packets (into current-packets m)]
+            (current-cb {:packets new-packets})
+            (recur current-cb
+                   current-file
+                   current-parse-chan
+                   new-packets)))))))
+
+(def reconciler
+  (om/reconciler
+   {:state app-state
+    :parser (om/parser {:read read :mutate mutate})
+    :send (send-to-chan send-chan)
+    ;;    :merge our-merge
+    :remotes [:file]}))
+(process-loop send-chan)
+
+(defn file-upload [ui]
+  (dom/input
+   #js {:type "file"
+        :onChange #(let [f (-> % .-target .-files js/Array.from (nth 0))]
+                     (om/update-query!
+                      ui
+                      (fn [s] (assoc-in s [:params :file] f))))}))
+
+(defn file-view [ui file]
+  (when file
+    (dom/div nil (str "File loaded: " (.-name file)))))
+
+(defmulti packet-view (fn [p] (aget p "packetType" :default)))
+(defmethod packet-view :default [p] (str/join ", " (js/Object.keys p)))
+(defmethod packet-view "setConVar" [p]
+  (->> (aget p "vars") js/Object.entries (map (fn [[k v]] (str k "=\"" v "\""))) (str/join " ")))
+(defmethod packet-view "netTick" [p]
+  (str "Tick: " (p "tick") " frameTime: " (p "frameTime")
+       " stdDev: " (p "stdDev")))
+(defmethod packet-view "gameEvent" [p]
+  (-> (aget p "event") js/JSON.stringify))
+;; (defmethod packet-view "packetEntities" [p]
+;;   (->> (p "entities")
+;;        (mapv (fn [ent]
+;;                (str (ent "entityIndex")
+;;                     "-" (get-in ent ["serverClass" "name"] nil)
+;;                     "-" (count (get ent "props" []))
+;;                     )))
+;;        (str/join "; ")
+;;        ))
+;; (defmethod packet-view "serverInfo" [p]
+;;   (str "frameTime: " (p "frameTime") " stdDev: " (p "stdDev")))
+(defmethod packet-view "consoleCmd" [p] (aget p "command"))
+(defmethod packet-view "print" [p] (aget p "value"))
+
+(defn packet-type-cell [offset data]
+  (fn [props-js]
+    (let [row-idx (- (aget props-js "rowIndex") offset)]
+      (jsx
+       Cell props-js
+       (if-let [row (get data row-idx)]
+         (str (aget row "packetType"))
+         "")))))
+
+(defn data-cell [offset data]
+  (fn [props-js]
+    (let [row-idx (- (aget props-js "rowIndex") offset)]
+      (jsx
+       Cell props-js
+       (if-let [row (get data row-idx)]
+         (packet-view row)
+         "")))))
+
+(defui RootComponent
+  static om/IQueryParams
+  (params [_] {:file nil :idx nil :offset 0 :limit 50})
+  static om/IQuery
+  (query [this]
+         '[:selected-idx :offset :limit
+           ;;           :display/visible-packets
+           :packets
+           :file/results
+           (:file/file {:file/file ?file})])
+  Object
+  (render [this]
+    (let [{:keys [file]} (om/get-params this)
+          {:keys
+           [selected-idx limit offset results title packets]}
+          (om/props this)
+
+          packets-count (count packets)
+          last-idx (max 0 (dec packets-count))
+          packet (when selected-idx (nth packets (min last-idx selected-idx) nil))
+          start (min offset last-idx)
+          end (min (+ limit offset) last-idx)
+          packets (subvec packets start end)]
+      (dom/div nil
+        (dom/h1 nil "Demo parser")
+        (dom/p nil results)
+        (file-upload this)
+        (when packets (dom/p nil (str "Loaded " packets-count " packets.")))
+        (when file (file-view this results))
+        (when packets
+          (jsx Table
+               #js {:rowHeight 32
+                    :headerHeight 32
+                    :rowsCount packets-count
+                    :onScrollStart
+                    (fn [x y]
+                      (let [new-offset (max 0 (- (int (/ y 32)) 10))]
+                        (om/transact! this `[(offset {:value ~new-offset})])))
+                    :onScrollEnd
+                    (fn [x y]
+                      (let [new-offset (max 0 (- (int (/ y 32)) 10))]
+                        (om/transact! this `[(offset {:value ~new-offset})])))
+                    :onRowClick
+                    (fn [_ new-idx]
+                      (om/transact! this `[(selected-idx {:value ~new-idx})]))
+                    :width 800
+                    :height 800}
+               (jsx Column
+                    #js {:width 150
+                         :header (jsx Cell #js {} "Packet Type")
+                         :cell (packet-type-cell offset packets)})
+               (jsx Column
+                    #js {:width (- 800 150)
+                         :header (jsx Cell #js {} "Data")
+                         :cell (data-cell offset packets)})))
+        (when packet
+          (dom/div nil (js/JSON.stringify packet)))))))
+
+
+(def root (om/factory RootComponent))
+
+(om/add-root! reconciler RootComponent (gdom/getElement "app"))
+
+(defn on-figwheel-reload [& args] (println "Figwheel reloaded!" args))
+
+
+
 
 ;; (defn process-loop [c]
 ;;   (go-loop [current-file nil]
@@ -160,39 +322,15 @@
 ;;              :packets []}))
 ;;       (recur file))))
 
-(defn process-loop [control-chan]
-  (go-loop [current-cb nil
-            current-file nil
-            current-parse-chan nil
-            current-packets nil]
-    (let [[m port] (alts! (remove nil? [control-chan current-parse-chan]))]
-      (cond
-        (= port control-chan)
-        (let [[file cb] m]
-          (println "Control chan:" file)
-          (if (and file (not= file current-file))
-            (do (when current-parse-chan (close! current-parse-chan))
-                (cb {:packets []})
-                (recur cb file (when file (parse file)) []))
-            (recur current-cb current-file current-parse-chan current-packets)))
 
-        (= port current-parse-chan)
-        (do
-          (let [new-packets (into current-packets m)]
-            (current-cb {:packets new-packets})
-            (recur current-cb
-                   current-file
-                   current-parse-chan
-                   new-packets)))))))
+;; (defn combine-merge-result [a b]
+;;   {:keys (concat (:keys a) (:keys b))
+;;    :next (merge (:next a) (:next b))
+;;    :tempids (concat (:tempids a) (:tempids b))})
 
-(defn combine-merge-result [a b]
-  {:keys (concat (:keys a) (:keys b))
-   :next (merge (:next a) (:next b))
-   :tempids (concat (:tempids a) (:tempids b))})
-
-(defn merge-packets [state new-packets]
-  {:keys [:packets]
-   :next (update state :packets #(vec (concat % new-packets)))})
+;; (defn merge-packets [state new-packets]
+;;   {:keys [:packets]
+;;    :next (update state :packets #(vec (concat % new-packets)))})
 
 ;; (defn our-merge [reconciler state novelty query]
 ;;   (let [{:keys [partial-packets]} novelty
@@ -201,123 +339,3 @@
 ;;     (if partial-packets
 ;;       (combine-merge-result results (merge-packets state partial-packets))
 ;;       results)))
-
-(def reconciler
-  (om/reconciler
-   {:state app-state
-    :parser (om/parser {:read read :mutate mutate})
-    :send (send-to-chan send-chan)
-    ;;    :merge our-merge
-    :remotes [:file]}))
-(process-loop send-chan)
-
-(defn file-upload [ui]
-  (dom/input
-   #js {:type "file"
-        :onChange #(let [f (-> % .-target .-files js/Array.from (nth 0))]
-                     (om/update-query!
-                      ui
-                      (fn [s] (assoc-in s [:params :file] f))))}))
-
-(defn file-view [ui file]
-  (when file
-    (dom/div nil (str "File loaded: " (.-name file)))))
-
-(defmulti packet-view (fn [p] (get p "packetType" :default)))
-(defmethod packet-view :default [p] (str/join ", " (keys p)))
-(defmethod packet-view "setConVar" [p]
-  (->> (p "vars") (map (fn [[k v]] (str k "=\"" v "\""))) (str/join " ")))
-(defmethod packet-view "netTick" [p]
-  (str "Tick: " (p "tick") " frameTime: " (p "frameTime")
-       " stdDev: " (p "stdDev")))
-(defmethod packet-view "gameEvent" [p]
-  (-> (p "event") clj->js js/JSON.stringify))
-(defmethod packet-view "packetEntities" [p]
-  (->> (p "entities")
-       (mapv (fn [ent]
-               (str (ent "entityIndex")
-                    "-" (get-in ent ["serverClass" "name"] nil)
-                    "-" (count (get ent "props" []))
-                    )))
-       (str/join "; ")
-       ))
-;; (defmethod packet-view "serverInfo" [p]
-;;   (str "frameTime: " (p "frameTime") " stdDev: " (p "stdDev")))
-(defmethod packet-view "consoleCmd" [p] (p "command"))
-(defmethod packet-view "print" [p] (p "value"))
-
-(defn packet-type-cell [offset data]
-  (fn [props-js]
-    (let [row-idx (- (aget props-js "rowIndex") offset)]
-      (jsx
-       Cell props-js
-       (if-let [row (get data row-idx)]
-         (str (row "packetType"))
-         "")))))
-
-(defn data-cell [offset data]
-  (fn [props-js]
-    (let [row-idx (- (aget props-js "rowIndex") offset)]
-      (jsx
-       Cell props-js
-       (if-let [row (get data row-idx)]
-         (packet-view row)
-         "")))))
-
-(defui RootComponent
-  static om/IQueryParams
-  (params [_] {:file nil :idx nil :offset 0 :limit 50})
-  static om/IQuery
-  (query [this]
-         '[:title
-           (:packet {:idx ?idx})
-           (:packets {:offset ?offset :limit ?limit})
-           :packets-count
-           :file/results
-           (:file/file {:file/file ?file})])
-  Object
-  (render [this]
-    (let [{:keys [results title packet packets packets-count]}
-          (om/props this)
-          {:keys [offset file]} (om/get-params this)]
-      (dom/div nil
-        (dom/h1 nil title)
-        (dom/p nil results)
-        (file-upload this)
-        (when packets
-          (dom/p nil
-                 (str "Loaded " packets-count " packets.")))
-        (when file (file-view this results))
-        (when packets
-          (jsx Table
-               #js {:rowHeight 32
-                    :headerHeight 32
-                    :rowsCount packets-count
-                    :onScrollEnd
-                    (fn [x y]
-                      (let [new-offset (max 0 (- (int (/ y 32)) 10))]
-                        (om/update-query!
-                         this #(assoc-in % [:params :offset] new-offset))))
-                    :onRowClick
-                    (fn [_ new-idx]
-                      (om/update-query!
-                       this #(assoc-in % [:params :idx] new-idx)))
-                    :width 800
-                    :height 800}
-               (jsx Column
-                    #js {:width 150
-                         :header (jsx Cell #js {} "Packet Type")
-                         :cell (packet-type-cell offset packets)})
-               (jsx Column
-                    #js {:width (- 800 150)
-                         :header (jsx Cell #js {} "Data")
-                         :cell (data-cell offset packets)})))
-        (when packet
-          (dom/div nil (js/JSON.stringify (clj->js packet))))))))
-
-
-(def root (om/factory RootComponent))
-
-(om/add-root! reconciler RootComponent (gdom/getElement "app"))
-
-(defn on-figwheel-reload [& args] (println "Figwheel reloaded!" args))
--- src/cljs/demtools/demo.cljs
+++ src/cljs/demtools/demo.cljs
@@ -2,15 +2,20 @@
   (:require [demtools.helpers :as h]
             [tf2demo]))
 
+(def obj-proto (js/Object.getPrototypeOf #js {}))
+
 (extend-type js/tf2demo.PacketEntity
   IEncodeClojure
-  (-js->clj [x opts] (h/plainify x)))
-(extend-type js/tf2demo.SendProp
-  IEncodeClojure
-  (-js->clj [x opts] (h/plainify x)))
-(extend-type js/tf2demo.SendPropDefinition
-  IEncodeClojure
-  (-js->clj [x opts] (h/plainify x)))
-(extend-type js/tf2demo.ServerClass
-  IEncodeClojure
-  (-js->clj [x opts] (h/plainify x)))
+  (-js->clj [x opts]
+    (js/Object.setPrototypeOf x obj-proto)
+    ))
+
+;; (extend-type js/tf2demo.SendProp
+;;   IEncodeClojure
+;;   (-js->clj [x opts] (h/plainify x)))
+;; (extend-type js/tf2demo.SendPropDefinition
+;;   IEncodeClojure
+;;   (-js->clj [x opts] (h/plainify x)))
+;; (extend-type js/tf2demo.ServerClass
+;;   IEncodeClojure
+;;   (-js->clj [x opts] (h/plainify x)))
